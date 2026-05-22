bea68f3a1d861215995b5c507d9ffea63eda90b4
unknown <naleina2@gmail.com>
2026-05-22 13:40:11 +0800

    feat membetulkan port dan menambahkan 3 logo di dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Logo from "./assets/hero.png";
+import LogoAtas from "./assets/react.svg";
+import ThunderLogo from "./assets/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Dashboard</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+return (
+<div className="container-box">
+<div style={{display:"flex", justifyContent: "center", alignItems: "center", gap: "20px", marginTop: "50px", marginBottom: "15px"}}>
 
-      <div className="ticks"></div>
+  {/* Logo 1: Logo */}
+  <img
+   src={reactLogo}
+   alt="Logo React"
+   style={{ width: "50px", height: "50px", animation: "Logo-spin infinite 20s linear" }}
+   />
+   {/* Logo 2: LogoAtas*/}
+   <img
+   src={LogoAtas}
+   alt="LogoAtas"
+   stylle={{ width: "90px", height: "auto" }}
+   />
+   {/* Logo 3: thunderLogo */}
+   <img
+   src={ThunderLogo}
+   alt="ThunderLogo"
+   style={{ width: "50px", height: "50px"}}
+   />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+  </div>  
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+{/*Judul Dashboard*/}
+<h1 style={{ fontSize: "50px", marginTop: "10px", marginBottom: "20px"}}>
+  Dashboard
+</h1>
+
+
+  {/* UI Bawaan Tombol Count */}  
+  <div className="card">
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+    
+  </div>
+
+  <p className="read-the-docs">
+    Click on the Vite and React logos to learn more
+  </p>
+  </div>
+);
 }
-
-export default App
+export default App;
